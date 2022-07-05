680cea0a8c29f27870dbe1620925153b81c4dc7e
kersiancoder <maxi.heijo@gmail.com>
2022-07-05 09:06:14 -0300

    separar los js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("hamburger__menu").classList.toggle("show__menu");
+    }
+    function hideMenu() {
+    document.getElementById("hamburger__menu").classList.remove("show__menu");
+    }
